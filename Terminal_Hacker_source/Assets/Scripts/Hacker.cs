76c584dd8dbfeb97255bef0e370e0ae23e233c90
DESKTOP-G07ROO4\alex- <alexvf93@gmail.com>
2018-01-19 16:38:08 +0100

    26. switch vs if

--- Terminal_Hacker_source/Assets/Scripts/Hacker.cs
+++ Terminal_Hacker_source/Assets/Scripts/Hacker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,10 @@ public class Hacker : MonoBehaviour
 {
     string playerName;
     int currentLevel;
+    string password;
+
+    string[] lv01Passwords = { "nazi", "franco", "gerard", "puigdemony" };
+    string[] lv02Passwords = { "musolini", "darth vader", "palpatine", "junqueras" };
 
     enum Screen
     {
@@ -22,57 +27,74 @@ public class Hacker : MonoBehaviour
         ShowMenu();
     }
 
-    void OnUserInput(string input)
+    void ShowMenu()
     {
-        if (currentScreen == Screen.MainMenu)
-        {
-            RunMainMenu(input);
-        }
+        currentScreen = Screen.MainMenu;
+        Terminal.ClearScreen();
+        Terminal.WriteLine("What do you want to hack?");
+        Terminal.WriteLine("Level 1: Library");
+        Terminal.WriteLine("Level 2: Police Station");
+        Terminal.WriteLine("Level 3: NASA");
+        Terminal.WriteLine("Select level:");
     }
 
-    void RunMainMenu(string input)
+    void OnUserInput(string input)
     {
         if (input == "menu")
         {
             ShowMenu();
         }
-        else if (input == "1")
+        else if (currentScreen == Screen.MainMenu)
         {
-            currentScreen = Screen.Password;
-            currentLevel = 1;
-            StartGame();
-}
-        else if (input == "2")
-        {
-            currentScreen = Screen.Password;
-            currentLevel = 2;
-            StartGame();
+            RunMainMenu(input);
         }
-        else if (input == "3")
+        else if (currentScreen == Screen.Password)
         {
-            currentScreen = Screen.Password;
-            currentLevel = 3;
+            CheckPassword(input);
+        }
+    }
+
+    void RunMainMenu(string input)
+    {
+        bool isValidNumber = (input == "1" || input == "2" || input == "3");
+        if (isValidNumber)
+        {
+            currentLevel = int.Parse(input);
             StartGame();
         }
         else
         {
-            Terminal.WriteLine("Please, type a valid text");
+            Terminal.WriteLine("Please, type a valid level");
         }
-    }
-
-    void ShowMenu()
-    {
-        Terminal.ClearScreen();
-        Terminal.WriteLine("What do you want to hack?");
-        Terminal.WriteLine("Level 1: Library");
-        Terminal.WriteLine("Level 2: Police Station");
-        Terminal.WriteLine("Select level:");
     }
 
     void StartGame()
     {
         currentScreen = Screen.Password;
+        Terminal.ClearScreen();
         Terminal.WriteLine("You choose level " + currentLevel);
         Terminal.WriteLine("Please enter your password");
+
+        switch (currentLevel)
+        {
+            case 1:
+                password = lv01Passwords[UnityEngine.Random.Range(0, lv01Passwords.Length)];
+                    break;
+            case 2:
+                password = lv02Passwords[UnityEngine.Random.Range(0, lv02Passwords.Length)];
+                break;
+        }
+    }
+
+    void CheckPassword(string input)
+    {
+        if (input == password)
+        {
+            Terminal.WriteLine("WELL DONE!");
+        }
+        else
+        {
+            Terminal.WriteLine("Sorry, wrong password!");
+        }
     }
 }
